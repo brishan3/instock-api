2ea4fc50d09d4a8615cb0c83ac75256f0b1adc82
Brishan King <brishan.king@gmail.com>
2022-03-14 16:07:51 -0700

    routing spelling mistake when requiring warehouse routes, server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const inventoryRoutes = require("./routes/inventoryRoutes");
-const warehouseRoutes = require("./routes/inventoryRoutes");
+const warehouseRoutes = require("./routes/warehouseRoutes");
 const PORT = process.env.PORT;
 
 
