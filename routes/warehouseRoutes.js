eaaf707eda58596d474c2db470522276b02bed58
Brishan King <brishan.king@gmail.com>
2022-03-14 16:03:23 -0700

    Folder structure and basic setup

---
+++ routes/warehouseRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+
+
+//
+// Write code here
+//
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,35 @@
+require('dotenv').config(); //load .env variables
+const express = require('express'); 
+const app = express();
+const cors = require('cors');
+const inventoryRoutes = require("./routes/inventoryRoutes");
+const warehouseRoutes = require("./routes/inventoryRoutes");
+const PORT = process.env.PORT;
+
+
+//
+// Middleware
+//
+app.use((req, res, next) => {
+  console.log(new Date(Date.now()).toLocaleString(), req.originalUrl);
+  next();
+})
+app.use(express.json()); // add req.body
+app.use(express.static('./public')); // add public folder for serving images
+app.use(cors()); // allow cross-origin resource sharing
+
+
+
+//
+// Routing
+//
+
+
+
+
+//
+// Listening
+//
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+})
