9e076f90b2c7dd53c72dfae59389f254b7fe6a1e
Brishan King <brishan.king@gmail.com>
2022-03-16 13:17:21 -0700

    Added routes to GET a warehouse by an ID, as well get the inventory for a warehouse by warehouse ID

--- routes/warehouseRoutes.js
+++ routes/warehouseRoutes.js
@@ -16,5 +16,28 @@ router.get('/', (req, res) => {
     });
 });
 
+// Returns data for a specific warehouse by a given ID
+router.get('/:id', (req, res) => {
+  fs.readFile('./data/warehouses.json', 'utf-8', (err,data) => {
+    if (err) {
+      console.log(err)
+      res.status(501).send('Error reading data file.')
+    } else {
+      const warehousesData = JSON.parse(data);
+      const reqWarehouse = warehousesData.find( (warehouse) => {
+        return warehouse.id === req.params.id
+      })
+      console.log(reqWarehouse);
+      if (reqWarehouse) {
+        res.status(201).json(reqWarehouse);
+      } else {
+        res.status(404).json({
+          message: 'No warehouse with that id exists',
+        })
+      }
+    }
+  });
+})
+
 
 module.exports = router;
