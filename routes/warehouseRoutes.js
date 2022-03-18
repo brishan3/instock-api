97fc25b96db5fc58491a91db42621942b79b4bed
Fahd Munir <fahd.munir.89@gmail.com>
2022-03-18 13:08:54 -0400

    delete req but nodemon crash

--- routes/warehouseRoutes.js
+++ routes/warehouseRoutes.js
@@ -104,4 +104,48 @@ router.put("/:id", (req, res) => {
     }
   });
 });
-module.exports = router;
+
+// router.delete("/:id", (req,res)=>{
+//   fs.readFile('./data/warehouses.json', 'utf-8', (err, data) => {
+//     if (err) {
+//       console.log(err)
+//       res.status(501).send(`Error deleting data file.`)
+//     } else {
+//       const warehousesData = JSON.parse(data);
+//       const id = warehousesData.findIndex((warehouse) => {
+//         return warehouse.id === req.params.id;
+//       });
+//     if (id) {
+//       fs.writeFile(
+//         "./data/warehouses.json",
+//         JSON.stringify(warehousesData), 
+//         () => {
+//           res.send("Warehouse deleted");
+//         }
+//       )
+//     }
+
+// read the json data, filter out the id that matches the id of the requested warehouse that the user wishes to delete. 
+// then write the new file with the updated data 
+router.delete("/:id", (req,res)=>{
+  fs.readFile('./data/warehouses.json', 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err)
+      res.status(501).send(`Error deleting data file.`)
+    } else {
+      const warehousesData = JSON.parse(data);
+      const updatedWarehouseData = warehousesData.filter((warehouse) =>
+        warehouse.id != req.params.id);
+      fs.writeFile(
+        "./data/warehouses.json",
+        JSON.stringify(updatedWarehouseData),
+        () => {
+          res.json(updatedWarehouseData).send("Warehouse deleted!");
+        }
+      );// add another else to have a 404 sent like in the put req?
+    }
+  });
+});
+
+
+module.exports = router
